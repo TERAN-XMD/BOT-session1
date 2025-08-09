247689f24ea090a243bc69ef0cb718d671af4263
TERAN-XMD <earnonline6k@gmail.com>
2025-08-09 14:34:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-__path = process.cwd()
+__path = process.cwd();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
+
+// Routes
 let qrRoute = require('./routers/qr');
 let pairRoute = require('./routers/pair');
 let validateRoute = require('./routers/validate');
+
+// Increase event listener limit
 require('events').EventEmitter.defaultMaxListeners = 1500;
 
+// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// API Endpoints
 app.use('/qr', qrRoute);
 app.use('/code', pairRoute);
-app.use('/giftedValidate.php', validateRoute);
+app.use('/teranxmdValidate.php', validateRoute);
 
-
+// Pages
 app.get('/validate', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'validate.html'));
 });
@@ -29,11 +36,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Start Server
 app.listen(PORT, () => {
-    console.log(`
-Deployment Successful!
+  console.log(`
+Deployment Successful! 🚀
 
- Gifted-Session-Server Running on http://localhost:` + PORT)
-})
+TERAN-XMD Session Server is running at:
+http://localhost:${PORT}
+`);
+});
 
-module.exports = app
+module.exports = app;
